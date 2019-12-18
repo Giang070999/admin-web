3638941dad395f4b5b72ef16fb1c0fb1bb106824
Nguyen Giang <giangsauhu3@gmail.com>
2019-12-18 23:51:43 +0700

    first commit

---
+++ routes/manager.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/charts', function(req, res, next) {
+  res.render('manager/charts');
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+    res.render('manager/login');
+  });
+
+  router.get('/register', function(req, res, next) {
+    res.render('manager/register');
+  });
+  router.get('/tables', function(req, res, next) {
+    res.render('manager/tables');
+  });
+  router.get('/products', function(req, res, next) {
+    res.render('manager/products');
+  });
+
+
+  router.get('/blank', function(req, res, next) {
+    res.render('manager/blank');
+  });
+module.exports = router;
